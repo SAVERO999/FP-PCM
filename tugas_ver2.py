2631b0959333e2ec5fe0936eb6b6d77cc985854e
SAVERO999 <167954392+SAVERO999@users.noreply.github.com>
2024-10-30 12:23:54 +0700

    Update tugas_ver2.py

--- tugas_ver2.py
+++ tugas_ver2.py
@@ -37,12 +37,12 @@ if selected == "Home":
     col1, col2 = st.columns(2)
 
     with col1:
-        st.image("IMG_2267.jpg", caption="\nReynard Prastya Savero (5023211042)", use_column_width=True, width=150, height=150)
-        st.image("IMG_2104.JPEG", caption="\nFrancisca Cindy Meilia Apsari (5023211021)", use_column_width=True, width=150)
-
+        st.image("IMG_2267.jpg", caption="\nReynard Prastya Savero (5023211042)", use_column_width=True, width=150, height=200)
+        st.image("IMG_2104.JPEG", caption="\nFrancisca Cindy Meilia Apsari (5023211021)", use_column_width=True, width=150, height=200)
+    
     with col2:
-        st.image("Screenshot 2024-10-30 122041.png", caption="\n Mavelyn Clarissa Tania (5023211004)", use_column_width=True, width=150,height=150)
-        st.image("IMG_20240410_113029.jpg", caption="\n Narika Shinta (5023211057)", use_column_width=True, width=150)
+        st.image("Screenshot 2024-10-30 122041.png", caption="\nMavelyn Clarissa Tania (5023211004)", use_column_width=True, width=150, height=200)
+        st.image("IMG_20240410_113029.jpg", caption="\nNarika Shinta (5023211057)", use_column_width=True, width=150, height=200)
 
 
 
