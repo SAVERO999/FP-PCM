867a56b58cc6c41b06d47216f78eec196b43c637
SAVERO999 <167954392+SAVERO999@users.noreply.github.com>
2024-10-31 00:34:57 +0700

    Update tugas_ver2.py

--- tugas_ver2.py
+++ tugas_ver2.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plt
+iimport matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 import numpy as np
 import scipy.ndimage as ndi
@@ -48,7 +48,7 @@ if 'threshold' not in st.session_state:
 
 
 with st.sidebar:
-    selected = option_menu("TUGAS 1", ["Home","Encyclopedia", "Pemrosesan dan Analisis Citra"], default_index=0)
+    selected = option_menu("TUGAS 1", ["Home","Encyclopedia", "Pemrosesan dan Analisis Citra,"Machine Learning"], default_index=0)
 
 if selected == "Home":
     st.title('Project FP Kelompok 2')
@@ -352,78 +352,3 @@ if selected == "Pemrosesan dan Analisis Citra ":
             # Menampilkan DataFrame dalam Streamlit
             st.write("Tabel Properti Objek:")
             st.dataframe(df1)
-
-if selected == "Machine Learning ":
-        st.markdown("<h1 style='text-align: center;'>📊 Machine Learning - K-Means Clustering</h1>", unsafe_allow_html=True)
-            
-        if 'area' in df1.columns:
-            # Normalisasi data antara 0-100
-            scaler = MinMaxScaler(feature_range=(0, 100))
-            df1[['area', 'major_axis_length', 'minor_axis_length']] = scaler.fit_transform(
-                df1[['area', 'major_axis_length', 'minor_axis_length']]
-            )
-    
-            # K-means clustering
-            kmeans = KMeans(n_clusters=4)  # Jumlah cluster dapat disesuaikan
-            df1['cluster'] = kmeans.fit_predict(df1[['area', 'major_axis_length', 'minor_axis_length']])
-    
-            # Plot 3D dengan label menggunakan Plotly
-            fig = px.scatter_3d(df1, x='area', y='major_axis_length', z='minor_axis_length',
-                                color='cluster', title="3D Plot After Clustering with K-Means",
-                                hover_name='label', color_continuous_scale='Viridis')
-            st.plotly_chart(fig)
-            
-
-
-
-
-
-
-
-
-
-
-        
-
-
-    
-    
-
-
-
-        
-    
-
-
-
-
-
-
- 
-
-
-        
-        
-
-
-
-
-
-
-        
-
-
-
-
-
-        
-        
-    
-
-
-
-
-    
-
-
-         
