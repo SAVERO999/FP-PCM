d47c137a57eec583f58f368f7d70e60570ea88ae
SAVERO999 <167954392+SAVERO999@users.noreply.github.com>
2024-10-30 12:16:49 +0700

    Update tugas_ver2.py

--- tugas_ver2.py
+++ tugas_ver2.py
@@ -9,7 +9,7 @@ from PIL import Image
 
 def resize_image(image_path, width, height):
     img = Image.open(image_path)
-    img = img.resize((width, height))
+    img = img.resize((width, height), Image.LANCZOS)  # Menggunakan LANCZOS untuk kualitas lebih baik
     return img
 # Fungsi untuk melakukan transformasi gambar dari RGB ke Grayscale dan inisialisasi kernel
 def process_image(image):
